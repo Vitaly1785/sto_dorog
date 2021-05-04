2fa06f889bafd30548c4c4a414a6e793902d2370
Vitaly1785 <korsar1785@yandex.ru>
2021-05-04 11:27:26 +0300

    Initial commit

---
+++ src/main/java/ru/petukhov/sto_dorog/StoDorogApplication.java
@@ -0,0 +1,13 @@
+package ru.petukhov.sto_dorog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoDorogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoDorogApplication.class, args);
+    }
+
+}
